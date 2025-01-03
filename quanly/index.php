e9c0c50c90c4b41d13371eb463020dfd9f45120d
bin2810 <bin200528@gmail.com>
2025-01-03 21:51:19 +0700

    tow

--- quanly/index.php
+++ quanly/index.php
@@ -3,11 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bassql.css">
-    <link rel="stylesheet" href="mainql.css">
+    <link rel="stylesheet" href="../quanly/css/bassql.css">
+    <link rel="stylesheet" href="../quanly/css/mainql.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="big">
+        <div class="background">
+            <div class="sidebar">
+                <div class="sidebar-img">
+                    <img src="../quanly/img/logo-ct-dark.png" alt="">
+                    <p>jnfnjkdf</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
