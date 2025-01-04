a0f8742c7fd7cff598f492103c3509c4814067a4
bin2810 <bin200528@gmail.com>
2025-01-04 20:01:45 +0700

    update

---
+++ admin/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../admin/css/bass.css">
+    <link rel="stylesheet" href="../admin/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="big">
+        <div class="background"></div>
+            <div class="sidebar">
+                <div class="sidebar-content">
+                    <div class="sidebar-header">
+                        <img src="  " alt="">
+                    </div>
+                    <div class="sidebar-main">
+                        <p><a href="">logo Danh mục</a></p>
+                        <p><a href="">logo Danh mục</a></p>
+                        <p><a href="">logo Danh mục</a></p>
+                        <p><a href="">logo Danh mục</a></p>
+                        <p><a href="">logo Danh mục</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="main">
+                <div class="main-nav">
+                    <div class="nav-col">
+                        <span class="nav-col-span">bloomscape</span><span> / bảng điều kiển</span>
+                        <p>Bảng điều kiển</p>
+                    </div>
+                    <div class="nav-col">
+                        <div class="input-login">
+                            <input type="text" name="" id="" placeholder="Nhập vào đây...">
+                            <input type="submit" value="Đăng nhập">
+                        </div>
+                    </div>
+                </div>
+                <div class="main-header">
+                    <div class="main-header-row">
+                        <div class="main-header-col">
+                            <div class="col-content-left">
+                                <p class="col-left-title">TIỀN HÔM NAY</p>
+                                <p class="col-left-money">$53,000</p>
+                                <p><span class="col-left-percent">+55%</span>  Kể từ hôm nay</p>
+                            </div>
+                            <div class="col-content-right">
+                                <i class="fa-solid fa-gear"></i>
+                            </div>
+                        </div>
+                        <div class="main-header-col">
+                            <div class="col-content-left">
+                                <p class="col-left-title">NGƯỜI DÙNG NGÀY NAY</p>
+                                <p class="col-left-money">2.300</p>
+                                <p><span class="col-left-percent">+3%</span> so với tuần trước</p>
+                            </div>
+                            <div class="col-content-right">
+                                <i class="fa-solid fa-earth-asia"></i>
+                            </div>
+                        </div>
+                        <div class="main-header-col">
+                            <div class="col-content-left">
+                                <p class="col-left-title">KHÁCH HÀNG MỚI</p>
+                                <p class="col-left-money">+3000</p>
+                                <p><span class="col-left-percent">-2%</span> so với quý trước</p>
+                            </div>
+                            <div class="col-content-right">
+                                <i class="fa-solid fa-address-card"></i>
+                            </div>
+                        </div>
+                        <div class="main-header-col">
+                            <div class="col-content-left">
+                                <p class="col-left-title">VIỆC BÁN HÀNG</p>
+                                <p class="col-left-money">$103.430</p>
+                                <p><span class="col-left-percent">+5%</span> so với tháng trước</p>
+                            </div>
+                            <div class="col-content-right">
+                                <i class="fa-solid fa-store"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-body">
+                    <div class="body-row1">
+                        <div class="body-row1-col1">
+                            <p>Tổng Quan Về Bán Hàng</p>
+                            <p>Tăng thêm 4% vào năm 2021</p>
+                        </div>
+                        <div class="body-row1-col2">
+                            <p>Tổng Quan Về Bán Hàng</p>
+                            <p>Tăng thêm 4% vào năm 2021</p>
+                        </div>
+                    </div>
+                    <div class="body-row2">
+                        <div class="body-row2-col1">
+                            <p>Tổng Quan Về Bán Hàng</p>
+                            <p>Tăng thêm 4% vào năm 2021</p>
+                        </div>
+                        <div class="body-row2-col2">
+                            <p>Tổng Quan Về Bán Hàng</p>
+                            <p>Tăng thêm 4% vào năm 2021</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
